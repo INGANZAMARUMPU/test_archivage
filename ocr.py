ad23d16ce26f264be6ab33be389795bb04bfffbc
INGANZAMARUMPU <jonkur96@gmail.com>
2023-03-19 12:09:37 +0200

    left PyPDF2 all document must become image first

--- ocr.py
+++ ocr.py
@@ -1,8 +1,31 @@
-import easyocr, sys
+import sys
+import docx2pdf
+import filetype
+from pathlib import Path
+from pdf2jpg import pdf2jpg
 
-reader = easyocr.Reader(['fr','fr'])
+fichier = Path(sys.argv[1])
 
-result = reader.readtext(sys.argv[1], batch_size=5, rotation_info=[90])
+kind = filetype.guess(fichier)
+print('File MIME type: %s' % kind.mime)
 
-print(" ".join([x[1] for x in result]))
-# print(result)
+pdf = None
+
+if(kind.mime == "application/pdf"):
+    pdf = fichier
+
+elif(kind.mime == "application/vnd.openxmlformats-officedocument.wordprocessingml.document"):
+    docx2pdf.convert(fichier.absolute(), f"{fichier.parent}.pdf")
+    pdf = Path(f"{fichier.parent}.pdf")
+
+if pdf:
+    result = pdf2jpg.convert_pdf2jpg(pdf, fichier.parent, pages="ALL")
+else:
+    print("incovertible")
+
+# pages_reader = PyPDF2.PdfReader(sys.argv[1])
+# print(len(list(pages_reader.pages)))
+# for page in pages_reader.pages[4:5]:
+#     for image in page.images:
+#         print()
+
