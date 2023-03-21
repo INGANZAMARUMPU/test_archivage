4a19ccca53ab55d405545379194fef424d57f9e1
INGANZAMARUMPU <jonkur96@gmail.com>
2023-03-21 13:54:00 +0200

    left docx

--- ocr.py
+++ ocr.py
@@ -1,8 +1,8 @@
-import sys
-import docx2pdf
-import filetype
 from pathlib import Path
 from pdf2jpg import pdf2jpg
+import sys
+import filetype
+import shutil
 
 fichier = Path(sys.argv[1])
 
@@ -12,20 +12,8 @@ print('File MIME type: %s' % kind.mime)
 pdf = None
 
 if(kind.mime == "application/pdf"):
-    pdf = fichier
-
-elif(kind.mime == "application/vnd.openxmlformats-officedocument.wordprocessingml.document"):
-    docx2pdf.convert(fichier.absolute(), f"{fichier.parent}.pdf")
-    pdf = Path(f"{fichier.parent}.pdf")
-
-if pdf:
-    result = pdf2jpg.convert_pdf2jpg(pdf, fichier.parent, pages="ALL")
+    result = pdf2jpg.convert_pdf2jpg(fichier, fichier.parent, pages="ALL")
 else:
-    print("incovertible")
-
-# pages_reader = PyPDF2.PdfReader(sys.argv[1])
-# print(len(list(pages_reader.pages)))
-# for page in pages_reader.pages[4:5]:
-#     for image in page.images:
-#         print()
-
+    new_path:Path = fichier.parent / (str(fichier.stem)+".pdf_dir/")
+    new_path.mkdir()
+    shutil.copyfile(str(fichier), new_path/('0_'+fichier.name))
