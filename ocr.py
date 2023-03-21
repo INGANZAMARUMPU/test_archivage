2029792e8d4d256d57630de0d6e4236b49f0a886
INGANZAMARUMPU <jonkur96@gmail.com>
2023-03-21 14:05:25 +0200

    limit accepted format

--- ocr.py
+++ ocr.py
@@ -6,10 +6,17 @@ import shutil
 
 fichier = Path(sys.argv[1])
 
+accepted_mine = [
+    "image/png",
+    "application/pdf",
+    "application/jpeg"
+]
+
 kind = filetype.guess(fichier)
 print('File MIME type: %s' % kind.mime)
 
-pdf = None
+if(not kind.mime in accepted_mine):
+    raise Exception("Format non prise en charge")
 
 if(kind.mime == "application/pdf"):
     result = pdf2jpg.convert_pdf2jpg(fichier, fichier.parent, pages="ALL")
