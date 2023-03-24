606c07abd3e88bda662aea1cc37c675a1afc23a2
INGANZAMARUMPU <jonkur96@gmail.com>
2023-03-25 00:02:11 +0200

    ok

--- ocr.py
+++ ocr.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import subprocess
 from pdf2jpg import pdf2jpg
 import sys
 import filetype
@@ -8,19 +9,33 @@ fichier = Path(sys.argv[1])
 
 accepted_mine = [
     "image/png",
-    "application/pdf",
-    "application/jpeg"
+    "image/jpeg",
+    "application/pdf"
 ]
 
 kind = filetype.guess(fichier)
-print('File MIME type: %s' % kind.mime)
 
-if(not kind.mime in accepted_mine):
-    raise Exception("Format non prise en charge")
+if not kind.mime in accepted_mine:
+    f = '\n '.join(accepted_mine)
+    print(f"{kind.mime} Non prise en charge.\n Seul les formats {f} \nsont prise en charge")
+    exit()
+
+image_path:Path = None
 
 if(kind.mime == "application/pdf"):
-    result = pdf2jpg.convert_pdf2jpg(fichier, fichier.parent, pages="ALL")
+    image_path = fichier.parent
+    result = pdf2jpg.convert_pdf2jpg(fichier, image_path, pages="ALL")
 else:
     new_path:Path = fichier.parent / (str(fichier.stem)+".pdf_dir/")
-    new_path.mkdir()
+    if not new_path.exists:
+        new_path.mkdir()
+    image_path = new_path
     shutil.copyfile(str(fichier), new_path/('0_'+fichier.name))
+
+for item in image_path.iterdir():
+    print(item)
+    print("_"*len(str(item)))
+    content = subprocess.check_output([
+        f"./tesseract", "-l", "fra", item, "stdout"
+    ]).decode()
+    print(content)
